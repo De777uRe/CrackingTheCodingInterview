100591e41ad3ddd1a15665d61d50a1c990a46780
De777uRe <willie5216@yahoo.com>
2017-04-08 17:32:32 -0500

    Added skeleton for solution to Anagrams challenge

---
+++ src/Main/Anagrams.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author willi
+ */
+public class Anagrams {
+    
+    public static int numberNeeded(String first, String second) {
+        int deletions = 0;
+        
+        return deletions;
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+    
+}
