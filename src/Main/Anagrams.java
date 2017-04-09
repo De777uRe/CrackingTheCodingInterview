769768b6a0d11a3a7037d60251654d9f471ec7fe
De777uRe <willie5216@yahoo.com>
2017-04-08 19:25:43 -0500

    Added solution to Anagrams challenge

--- src/Main/Anagrams.java
+++ src/Main/Anagrams.java
@@ -5,11 +5,7 @@
  */
 package Main;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 /**
  *
@@ -18,7 +14,27 @@ import java.util.regex.*;
 public class Anagrams {
     
     public static int numberNeeded(String first, String second) {
+        StringBuilder firstBuilder = new StringBuilder(first);
+        StringBuilder secondBuilder = new StringBuilder(second);
         int deletions = 0;
+        
+        if (first.length() < second.length()) {
+            firstBuilder = new StringBuilder(second);
+            secondBuilder = new StringBuilder(first);
+        }
+
+        char selectedChar;
+        for (int i = 0; i < firstBuilder.length(); i++) {
+            selectedChar = firstBuilder.charAt(i);
+            if (secondBuilder.toString().contains(Character.toString(selectedChar))) {
+                secondBuilder.deleteCharAt(secondBuilder.toString().indexOf(selectedChar));
+            }
+            else {
+                deletions++;
+            }
+        }
+        
+        deletions += secondBuilder.length();
         
         return deletions;
     }
